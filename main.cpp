c9b1d9d278fc69e1442afff67bcfe309ef2cda8d
Acherontas <145214445+Acherontas@users.noreply.github.com>
2025-09-24 19:25:12 +0300

    Add files via upload
    added for loop a cheap one
    without class and other 
    just watch the results

--- main.cpp
+++ main.cpp
@@ -6,67 +6,64 @@
 
 using namespace std;
 
-int main(int argc,char** argv)
-{
     long double mn;
     long double niine=99980001;
+    long double n_nei[12]={
+                           980001,
+                           9980001,
+                           99980001,
+                           999980001,
+                           9999980001,
+                           99999980001,
+                           999999980001,
+                           9999999980001,
+                           99999999980001,
+                           999999999980001,
+                           9999999999980001
+                           };
+    long double rst_d=0;
+    long double fractpart;
+    long double intpart;
+    long double sqtr=0;
+    long double xmln=0;
+    long double rxml=0;
     int preci=0;
     int choo_ser=0;
-    cout<<"hello voyeristic behaviours have fun \n";
-    cout<<"enter ur number , ur precision , ur nines pattern .Press 1 to change or 0 to leave it\n";
-    cout<<"as it the default value is 99980001\n";
-    mn=stold(argv[1]);
-    preci=stoi(argv[2]);
-    choo_ser=stoi(argv[3]);
-    string mnb;
-    mnb.clear();
-    mnb+=to_string(mn);
     int nmlet=0;
-    nmlet=mnb.size()-1;
-   // cout<<mnb<<"\n";
-   // cout<<"ur main number " << std::setprecision(preci) << mn << " with size of " << nmlet <<"\n";
-    if (choo_ser==1){
-       //cout<<"enter ur new nine pattern to use \n";
-       //cin>>niine;
-       //cout<<"ur new nine pattern will be ::== " << niine <<"\n";
-       niine=stold(argv[4]);
-    }
-    stringstream mti;
-    string inmn;
-    inmn.clear();
-    mti<<std::setprecision(preci)<<mn;
-    inmn+=mti.str();
     int lin=0;
-    lin=inmn.size();
-    long double rst_d=0;
-    rst_d=(long double)mn/(long double)niine;
+    int st=0;
+    int fnd=0;
+    int let=0;
+    int xl=10;
+    int tmc=0;
+    string lct;
+    string mct;
+    string slk;
+    string inmn;
+    string mnb;
+
+
+int repeater(long double nine){
+
+    rst_d=(long double)mn/(long double)nine;
     cout<<"\n";
-    cout<<"of " << std::setprecision(preci) << mn << " / " << std::setprecision(preci) << niine << " ::== ";
+    cout<<"of " << std::setprecision(preci) << mn << " / " << std::setprecision(preci) << nine << " ::== ";
     cout<< std::setprecision(preci) << rst_d <<"\n";
     cout<<"\n";
-    long double fractpart,intpart;
     fractpart=modf(rst_d,&intpart);
     cout<<std::setprecision(preci) << intpart << " + " << std::setprecision(preci) << fractpart << " ::== " << std::setprecision(preci)<<rst_d <<"\n";
     cout<<"\n";
-    long double sqtr=0;
     sqtr=sqrt(rst_d);
     cout<<"The main sqrt " << std::setprecision(preci) << sqrt(mn) <<" and ";
     cout<<"ur sqrt is " << std::setprecision(preci) << sqtr <<"\n";
     cout<<"\n";
-    string slk;
     slk.clear();
     stringstream smk;
     smk<<std::setprecision(preci) << sqtr;
     slk+=smk.str();
-    int let=0;
     let=slk.size()-1;
     cout<<"The String Representation is " << slk <<" with size " << let <<"\n";
-    int xl=10;
-    long double xmln=0;
-    long double rxml=0;
-    string lct;
-    int st=0;int fnd=0;
-    string mct;int tmc=0;
+    st=0;fnd=0;
     for(int i=1;i<=let+1;i++)
     {
       xl=10;
@@ -93,6 +90,44 @@ int main(int argc,char** argv)
       if(st==1 && fnd==0){cout<<" -------> " << rxml <<" " << " vs " << mn << " " << lct <<"\n"; st=0; fnd=1;}
       cout<<"\n";
       if(st==1){break;}
+    }
+return 0;
+}
+
+
+
+int main(int argc,char** argv)
+{
+    cout<<"hello voyeristic behaviours have fun \n";
+    cout<<"enter ur number , ur precision , ur nines pattern .Press 1 to change,2 for table or 0 to leave it\n";
+    cout<<"as it the default value is 99980001\n";
+    mn=stold(argv[1]);
+    preci=stoi(argv[2]);
+    choo_ser=stoi(argv[3]);
+    mnb.clear();
+    mnb+=to_string(mn);
+    nmlet=mnb.size()-1;
+   // cout<<mnb<<"\n";
+   // cout<<"ur main number " << std::setprecision(preci) << mn << " with size of " << nmlet <<"\n";
+    if (choo_ser==1){
+       niine=stold(argv[4]);
+       repeater(niine);
+    }
+    stringstream mti;
+    inmn.clear();
+    mti<<std::setprecision(preci)<<mn;
+    inmn+=mti.str();
+    lin=inmn.size();
+    if(choo_ser==2){
+       for(int i=0;i<=12;i++){
+         cout<<"\n";
+         cout<<"------------------------------\n";
+         repeater(n_nei[i]);
+         cout<<"------------------------------\n";
+       }
+    }
+    if(choo_ser==0){
+      repeater(niine);
     }
     return 0;
 }
