4ac6b87eb53e6e6aa0f3daa84e1d1de455b0981b
Acherontas <145214445+Acherontas@users.noreply.github.com>
2025-09-24 10:01:53 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <iomanip>
+#include "math.h"
+#include <thread>
+#include "sstream"
+
+using namespace std;
+
+int main(int argc,char** argv)
+{
+    long double mn;
+    long double niine=99980001;
+    int preci=0;
+    int choo_ser=0;
+    cout<<"hello voyeristic behaviours have fun \n";
+    cout<<"enter ur number , ur precision , ur nines pattern .Press 1 to change or 0 to leave it\n";
+    cout<<"as it the default value is 99980001\n";
+    mn=stold(argv[1]);
+    preci=stoi(argv[2]);
+    choo_ser=stoi(argv[3]);
+    string mnb;
+    mnb.clear();
+    mnb+=to_string(mn);
+    int nmlet=0;
+    nmlet=mnb.size()-1;
+   // cout<<mnb<<"\n";
+   // cout<<"ur main number " << std::setprecision(preci) << mn << " with size of " << nmlet <<"\n";
+    if (choo_ser==1){
+       cout<<"enter ur new nine pattern to use \n";
+       cin>>niine;
+       cout<<"ur new nine pattern will be ::== " << niine <<"\n";
+    }
+    cout<<"The main sqrt " << std::setprecision(preci) << sqrt(mn) <<"\n";
+    long double rst_d=0;
+    rst_d=(long double)mn/(long double)niine;
+    cout<<"The result is " << std::setprecision(preci) << rst_d <<"\n";
+    long double fractpart,intpart;
+    fractpart=modf(rst_d,&intpart);
+    cout<<std::setprecision(preci) << intpart << " + " << std::setprecision(preci) << fractpart << " ::== " << std::setprecision(preci)<<rst_d <<"\n";
+    long double sqtr=0;
+    sqtr=sqrt(rst_d);
+    cout<<"ur sqrt is " << std::setprecision(preci) << sqtr <<"\n";
+    string slk;
+    slk.clear();
+    stringstream smk;
+    smk<<std::setprecision(preci) << sqtr;
+    slk+=smk.str();
+    int let=0;
+    let=slk.size()-1;
+    cout<<"The String Representation is " << slk <<" with size " << let <<"\n";
+    int xl=10;
+    long double xmln=0;
+    long double rxml=0;
+    string lct;
+    int st=0;int fnd=0;
+    for(int i=1;i<=let+1;i++)
+    {
+      xl=10;
+      lct.clear();
+      xl=pow(xl,i);
+      xmln=sqtr*xl;
+      rxml=xmln*xmln;
+      fractpart=modf(rxml,&intpart);
+      if(preci<i){preci=i;}
+      //cout<<std::setprecision(preci) << intpart << " + " << fractpart << " ::== " << rxml <<"\n";
+      if(intpart>=mn){lct+=" ~= LOCATED";st=1;}
+      cout<<"@i " <<std::setprecision(preci) << xl << " rst " << std::setprecision(preci) << xmln << " "  ;
+      if(st==0){cout<<" -------> "<<std::setprecision(preci) << rxml << " " ;}
+      if(st==1 && fnd==0){cout<<" -------> " << rxml <<" " << " vs " << mn << " " << lct <<"\n"; st=0; fnd=1;}
+      cout<<"\n";
+      if(st==1){break;}
+    }
+    return 0;
+}
